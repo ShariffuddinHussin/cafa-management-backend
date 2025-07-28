1389ba2dab20ff8ed7eea7d20cddb77a4a9c6c25
Muhammad Shariffuddin bin Hussin <shariff@samurai2k.local>
2025-07-28 11:23:12 +0800

    RoleUser model

--- app/Models/RoleUser.php
+++ app/Models/RoleUser.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class RoleUser extends Model
 {
-    //
+    use HasFactory;
+    protected $table = "role_user";
+    protected $fillable = [
+        'user_id',
+        'role_id',
+        'assigned_by'
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+    public function assigner()
+    {
+        return $this->belongsTo(User::class, 'assigned_by');
+    }
 }
